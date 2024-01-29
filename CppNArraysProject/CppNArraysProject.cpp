5e4c6e11bd7f7663419ae071f2eb791338c07cdb
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-01-29 21:14:16 +0300

    29-01-24

--- CppNArraysProject/CppNArraysProject.cpp
+++ CppNArraysProject/CppNArraysProject.cpp
@@ -6,7 +6,9 @@ const int cols{ 7 };
 void MatrixInit(int matrix[][cols], int rows);
 void MatrixPrint(int matrix[][cols], int rows);
 void MatrixShiftLeft(int matrix[][cols], int rows, int steps);
-
+void MatrixShiftRight(int matrix[][cols], int rows, int steps);
+void MatrixShiftUp(int matrix[][cols], int rows, int steps);
+void MatrixShiftDown(int matrix[][cols], int rows, int steps);
 
 int main()
 {
@@ -19,13 +21,10 @@ int main()
     MatrixPrint(matrix, rows);
     std::cout << "\n";
 
-    MatrixShiftLeft(matrix, rows, 2);
+    MatrixShiftUp(matrix, rows, 2);
 
     MatrixPrint(matrix, rows);
     std::cout << "\n";
-
-    bool f{ true };
-    std::cout << std::boolalpha << f << "\n";
 
     //int count{};
 
@@ -68,5 +67,56 @@ void MatrixShiftLeft(int matrix[][cols], int rows, int steps)
             matrix[i][cols - 1] = temp;
         }
     }
-    
+}
+
+void MatrixShiftRight(int matrix[][cols], int rows, int steps)
+{
+    steps %= cols;
+    int temp;
+
+    for (int i{}; i < rows; i++)
+    {
+        for (int s{}; s < steps; s++)
+        {
+            int temp{ matrix[i][cols - 1] };
+            for (int j{ cols - 1 }; j > 0; j--)
+                matrix[i][j] = matrix[i][j - 1];
+            matrix[i][0] = temp;
+        }
+    }
+}
+
+void MatrixShiftUp(int matrix[][cols], int rows, int steps)
+{
+    steps %= rows;
+    int temp;
+
+    for (int j{}; j < cols; j++)
+    {
+        for (int s{}; s < steps; s++)
+        {
+            temp = matrix[0][j];
+            for (int i{}; i < rows - 1; i++)
+                matrix[i][j] = matrix[i + 1][j];
+            matrix[rows - 1][j] = temp;
+        }
+    }
+}
+
+void MatrixShiftDown(int matrix[][cols], int rows, int steps)
+{
+    steps %= rows;
+    int temp;
+
+    for (int j{}; j < cols; j++)
+    {
+        for (int s{}; s < steps; s++)
+        {
+            temp = matrix[rows - 1][j];
+            for (int i{ rows - 1 }; i > 0; i--)
+                matrix[i][j] = matrix[i - 1][j];
+            matrix[0][j] = temp;
+        }
+    }
+
 }
